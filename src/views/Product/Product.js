94235082fca60a049cd491bef3c83e9a6acefaf7
Anh Nguyen Hoang <anhnh@tech.est-rouge.com>
2025-03-02 10:24:25 +0700

    Add product allow 1 image 1 variant

--- src/views/Product/Product.js
+++ src/views/Product/Product.js
@@ -187,7 +187,7 @@ const Product = () => {
                               <td>{v.p_name}</td>
                               <td>
                                 <img
-                                  src={v.p_images[0].url}
+                                  src={v.variants[0].image.url}
                                   alt="Product"
                                   className="img-thumbnail"
                                   style={{ height: "100px" }}
