89d899f466433a54e48073f507e0743041f3e664
Anh Nguyen Hoang <anhnh@tech.est-rouge.com>
2025-04-04 23:26:28 +0700

    Fix empty data

--- src/views/Product/Product.js
+++ src/views/Product/Product.js
@@ -187,7 +187,7 @@ const Product = () => {
                               <td>{v.p_name}</td>
                               <td>
                                 <img
-                                  src={v.variants[0].images[0].url}
+                                  src={v.variants[0].images.length > 0 ? v.variants[0].images[0].url : ""}
                                   alt="Product"
                                   className="img-thumbnail"
                                   style={{ height: "100px" }}
